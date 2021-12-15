0d3c6ee3da6ba2d382ba104206153aba6fff338c
HSC-1 <kreaoo13@gmail.com>
2021-12-16 00:25:00 +0900

    포스트 게시 작업 문제사항 pagination categoru 작동 X

--- first_project/models.py
+++ first_project/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 from django.shortcuts import render
+from django.contrib.auth.models import User
+class Category(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+    def get_url(self):
+        return f'/blog/category/{self.slug}/'
+
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 # Create your models here.
 class Post(models.Model):
@@ -8,10 +26,16 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now= True)
     img = models.ImageField(upload_to = 'blog/images/%Y/%m/%d/', blank = True)
-
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank= True)
     def __str__(self) -> str:
         return f'[{self.pk}]{self.title}'
 
 
     def get_url(self):
         return f'/blog/{self.pk}/' 
+
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/' 
+
+    
--- first_project/views.py
+++ first_project/views.py
@@ -1,20 +1,54 @@
 # from django.shortcuts import render
-from .models import Post
-from django.views.generic import ListView, DetailView
+from django.db.models import fields
+from django.shortcuts import get_object_or_404, render
+from .models import Category, Post
+from django.views.generic import ListView, DetailView, CreateView
+
+from first_project import models
 # from django.views.generic import ListView
 # from .models import Post
 # Create your views here.
 
 
-class PostList(ListView):
+
+class PostCreate(CreateView):
     model = Post
+    fields = ['title','content', 'img', 'category']
+
+class PostList(ListView):
+    paginate_by = 2
+    model = Post
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['no_category_count'] = Post.objects.filter(category=None).count()
+        return context
 
 
 class PostDetail(DetailView):
     model = Post
 
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'first_project/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_count': Post.objects.filter(category=None).count(),
+            'category':category,
+        }
+    )
 # def index(request):
-#     posts = Post.objects.all().order_by('pk')
+#     posts = Post.objects.al l().order_by('pk')
 #     return render(
 #         request,
 #         'blog/index.html',
